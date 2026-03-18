e477b1f33d6a4e2bab95b545f18cf99f1e77d97b
Joeri <51537901+j-jurion@users.noreply.github.com>
2026-03-18 10:38:32 +0100

    add typer

---
+++ dev_scripts/example.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import cv2 as cv
+import numpy as np
+import typer
+
+from src.imip import IMIP
+
+app = typer.Typer()
+
+
+def process_image(image: np.ndarray) -> np.ndarray:
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    imip.debug(gray, "Gray Image")
+    blurred = cv.GaussianBlur(gray, (5, 5), 4)
+    imip.debug(blurred, "Blurred Image")
+    _, thresholded = cv.threshold(blurred, 50, 255, cv.THRESH_BINARY)
+    imip.debug(thresholded, "Thresholded Image")
+    return thresholded
+
+
+@app.command()
+def main(
+    image_folder: Path = typer.Argument(
+        ..., help="Path to the folder containing images to process"
+    ),
+    output_directory: Path = typer.Option(
+        None, help="Path to the output directory for debug images"
+    ),
+):
+    global imip
+    imip = IMIP(
+        images=image_folder,
+        output_directory=output_directory,
+    )
+    imip.debug_fn(process_image, watch_file=Path(__file__))
+
+
+if __name__ == "__main__":
+    app()
