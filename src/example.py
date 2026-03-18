4847469a2f84b70157e5656e8c6890518cada647
Joeri <51537901+j-jurion@users.noreply.github.com>
2026-03-18 08:35:06 +0100

    add __file__

--- src/example.py
+++ src/example.py
@@ -9,9 +9,9 @@ from imip import IMIP
 def process_image(image: np.ndarray) -> np.ndarray:
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     imip.debug(gray, "Gray Image")
-    blurred = cv.GaussianBlur(gray, (5, 5), 2)
+    blurred = cv.GaussianBlur(gray, (5, 5), 4)
     imip.debug(blurred, "Blurred Image")
-    _, thresholded = cv.threshold(blurred, 50, 255, cv.THRESH_BINARY)
+    _, thresholded = cv.threshold(blurred, 100, 255, cv.THRESH_BINARY)
     imip.debug(thresholded, "Thresholded Image")
     return thresholded
 
@@ -27,4 +27,4 @@ def analyse_image(image: np.ndarray) -> int:
 if __name__ == "__main__":
     image_folder = Path("./images")
     imip = IMIP(images=image_folder, output_directory=Path("./debug_images"), save_debug_images=True)
-    imip.debug_fn(process_image, watch_file=Path("src/example.py"))
+    imip.debug_fn(process_image, watch_file=Path(__file__))
